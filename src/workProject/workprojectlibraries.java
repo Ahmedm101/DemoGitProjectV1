f2e732f9c6f3f38ed597d7323f42f41a6d8afd94
malih <malih@192.168.1.224>
2023-01-13 17:25:29 -0600

    this is the java project for github this is the lead and this is the first v1project. you have to work in it

---
+++ src/workProject/workprojectlibraries.java
@@ -0,0 +1,14 @@
+package workProject;
+
+public class workprojectlibraries {
+
+	public static void main(String[] args) {
+		int x=5;
+		String y = "hello";
+	    
+				String z = x+y;
+				System.out.println(z);
+
+	}
+
+}
